e1e608701628319e60a7d9ec9720f23bddabd2c1
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2018-02-06 23:40:26 +0000

    configurable welcome message

--- app/src/main/java/com/ivarprudnikov/customeruploader/activities/MainActivity.java
+++ app/src/main/java/com/ivarprudnikov/customeruploader/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ivarprudnikov.customeruploader.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ivarprudnikov.customeruploader.R;
 
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    @BindView(R.id.home_content)
+    TextView homeContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        homeContent.setText(sharedPref.getString("welcome", null));
     }
 
     @OnClick(R.id.fab)
@@ -39,19 +51,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             this.startActivity(intent);
--- app/src/main/java/com/ivarprudnikov/customeruploader/settings/MainPreferenceFragment.java
+++ app/src/main/java/com/ivarprudnikov/customeruploader/settings/MainPreferenceFragment.java
@@ -1,10 +1,13 @@
 package com.ivarprudnikov.customeruploader.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 
-public class MainPreferenceFragment extends PreferenceFragment {
+public class MainPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,9 +16,39 @@ public class MainPreferenceFragment extends PreferenceFragment {
         PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(getActivity());
         setPreferenceScreen(screen);
 
-        // if flavour 01 send using email
+        PreferenceCategory category = new PreferenceCategory(screen.getContext());
+        category.setTitle("Home screen tweaks");
+        screen.addPreference(category);
 
-        // if flavour 02 send to firebase
+        EditTextPreference greeting = new EditTextPreference(screen.getContext());
+        greeting.setKey("welcome");
+        greeting.setDialogTitle("Welcome message");
+        greeting.setTitle("Welcome message");
+        greeting.setDialogMessage("Provide short message");
+        greeting.setDefaultValue("Modify me in configuration screen");
+        greeting.setSummary("");
+        category.addPreference(greeting);
+
+        onSharedPreferenceChanged(null, "");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        EditTextPreference pref = (EditTextPreference) findPreference("welcome");
+        pref.setSummary("dummy"); // required or will not update
+        pref.setSummary(pref.getText());
     }
 
 }
