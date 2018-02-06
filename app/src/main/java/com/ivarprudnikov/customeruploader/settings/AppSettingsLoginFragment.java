54f0fc31f4d4eec75c23707509d622dda3350051
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2018-02-06 22:58:45 +0000

    use Butterknife in DialogFragment

--- app/src/main/java/com/ivarprudnikov/customeruploader/settings/AppSettingsLoginFragment.java
+++ app/src/main/java/com/ivarprudnikov/customeruploader/settings/AppSettingsLoginFragment.java
@@ -7,8 +7,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +15,22 @@ import android.widget.TextView;
 
 import com.ivarprudnikov.customeruploader.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AppSettingsLoginFragment extends DialogFragment {
 
     public static String TAG = "AppSettingsLoginFragment";
+
+    @BindView(R.id.passwordMessage)
+    TextView message;
+    @BindView(R.id.password)
+    EditText pass;
+    @BindView(R.id.buttonCancel)
+    Button buttonCancel;
+    @BindView(R.id.buttonLogin)
+    Button buttonLogin;
 
     public interface OnDetachListener {
         void onLoginFragmentDetached();
@@ -51,12 +62,8 @@ public class AppSettingsLoginFragment extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        final LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.settings_login_dialog, null);
-
-        final TextView message = (TextView) layout.findViewById(R.id.passwordMessage);
-        final EditText pass = (EditText) layout.findViewById(R.id.password);
+        final LinearLayout layout = (LinearLayout) getActivity().getLayoutInflater().inflate(R.layout.settings_login_dialog, null);
+        ButterKnife.bind(this, layout);
 
         pass.addTextChangedListener(new TextWatcher() {
             @Override
@@ -73,32 +80,29 @@ public class AppSettingsLoginFragment extends DialogFragment {
             }
         });
 
-        final Button buttonCancel = (Button) layout.findViewById(R.id.buttonCancel);
-        buttonCancel.setOnClickListener(v -> {
-            Log.d(TAG, "onCancel");
-            getDialog().dismiss();
-            getActivity().finish();
-        });
-
-        final Button buttonLogin = (Button) layout.findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener((View view) -> {
-            Log.d(TAG, "onLogin");
-            if (onAuthenticateListener != null) {
-                boolean result = onAuthenticateListener.onLoginFragmentAuthenticate(pass.getText().toString());
-                if (result) {
-                    message.setVisibility(View.GONE);
-                    getDialog().dismiss();
-                    Log.d(TAG, "Dismiss");
-                } else {
-                    message.setText(getResources().getString(R.string.pref_security_pass_failed_msg));
-                    message.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-
         builder.setView(layout);
 
         return builder.create();
+    }
+
+    @OnClick(R.id.buttonCancel)
+    public void onCancelClick(View view){
+        getDialog().dismiss();
+        getActivity().finish();
+    }
+
+    @OnClick(R.id.buttonLogin)
+    public void onLoginClick(View view){
+        if (onAuthenticateListener != null) {
+            boolean result = onAuthenticateListener.onLoginFragmentAuthenticate(pass.getText().toString());
+            if (result) {
+                message.setVisibility(View.GONE);
+                getDialog().dismiss();
+            } else {
+                message.setText(getResources().getString(R.string.pref_security_pass_failed_msg));
+                message.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
 
